830418821011c73b72208d811d57ad24c23799cf
sazidahmednassir <sazidahmednassir@gmail.com>
2022-05-22 20:55:01 -0800

    firebase setup for this project

---
+++ src/Pages/Blog/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h2 class="text-2xl ...">The quick brown fox ...</h2>
+        </div>
+    );
+};
+
+export default Blog;
