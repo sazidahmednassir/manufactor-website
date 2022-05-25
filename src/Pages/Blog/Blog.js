33ba8ff639760942f537463598bb09d0c5790c63
sazidahmednassir <sazidahmednassir@gmail.com>
2022-05-25 11:44:27 -0800

    Blog Created

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React from "react";
 
 const Blog = () => {
-    return (
-        <div>
-            <h2 class="text-2xl ...">The quick brown fox ...</h2>
+  return (
+    <div>
+      <div class="card lg:card-side bg-base-100 shadow-xl my-2">
+        <div class="card-body ">
+          <h2 class="card-title ">
+            How will you improve the performance of a React Application?
+          </h2>
+          <p>
+            There are a few ways to improve the performance of a React
+            application. One way is to use a library like react-router-dom,
+            which helps to optimize routes. You can also use a library like
+            react-cache, which helps to cache data. Additionally, you can use a
+            library like react-transform, which helps to optimize your code.
+            Finally, you can use a tool like webpack, which helps to bundle your
+            files.
+          </p>
         </div>
-    );
+      </div>
+      <div class="card lg:card-side bg-base-100 shadow-xl my-2">
+        <div class="card-body ">
+          <h2 class="card-title ">
+          What are the different ways to manage a state in a React application?
+          </h2>
+          <p>
+          There are a few different ways to manage state in a React application. One way is to use the built-in state management provided by React. This is usually done by using the constructor function to create an instance of the React component, and then using the this.state property to store the state information.Another way to manage state is to use a third-party library such as Redux or MobX.
+          </p>
+        </div>
+      </div>
+      <div class="card lg:card-side bg-base-100 shadow-xl my-2">
+        <div class="card-body ">
+          <h2 class="card-title ">
+          How does prototypical inheritance work?
+          </h2>
+          <p>
+          Prototypical inheritance is a type of inheritance where an object can inherit the properties of another object. This is done by using a prototype chain, which is a series of objects that are linked together. When an object is created, it is given a prototype, which is the first object in the prototype chain. If the object wants to inherit a property from another object, it will look for that property in its prototype.
+          </p>
+        </div>
+      </div>
+      <div class="card lg:card-side bg-base-100 shadow-xl my-2">
+        <div class="card-body ">
+          <h2 class="card-title ">
+          Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts
+          </h2>
+          <p>
+          There are a few reasons why you wouldn't want to set the state directly in React. First, it makes your code difficult to read and understand. Second, it makes it more difficult to debug errors. Third, it can lead to unexpected behavior if multiple components are trying to update the same state variable. Finally, it can be more difficult to test your code if you're not able to mock the state variable.
+          </p>
+        </div>
+      </div>
+      <div class="card lg:card-side bg-base-100 shadow-xl my-2">
+        <div class="card-body ">
+          <h2 class="card-title ">
+          What is a unit test? Why should write unit tests?
+          </h2>
+          <p>
+          A unit test is a software testing technique that tests individual units of source code. A unit is the smallest testable part of an application. By testing individual units, you can verify that each unit functions as expected and that the interactions between units are correct. Unit tests also provide documentation about how the code works.Unit tests are important because they help you verify that your code works as expected.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/Pages/Products/PurchasePage.js
+++ src/Pages/Products/PurchasePage.js
@@ -95,11 +95,11 @@ const PurchasePage = () => {
         <div class="card-body">
           <h2 class="card-title font-bold"> Product Name : {product?.name}</h2>
           <h3 class="card-title font-bold text-red-500">
-            {" "}
+            
             Product Price : {product?.price}
           </h3>
           <h3 class="card-title text-green-500">
-            {" "}
+            
             Available Quantity : {product.availableQuantity}
           </h3>
           <h3 class="card-title font-bold text-yellow-500">Minimum order quantity : {product.minimum}</h3>
